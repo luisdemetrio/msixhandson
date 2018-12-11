a868ce8274f1b3f95e740c082f57cdbf22248d88
Luis Demetrio <luis.demetrio@outlook.com>
2018-12-10 22:25:17 -0200

    1
    1

---
+++ CustomerList.WPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using CustomerList.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomerList.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public CustomerListPageViewModel ViewModel { get; set; } = new CustomerListPageViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string versionNumber = string.Empty;
+
+            string file = "Version.txt";
+
+            if (System.IO.File.Exists(file)) {
+                versionNumber = System.IO.File.ReadAllText(file);
+            }
+
+            txtVersionNumber.Text = $"Version: {versionNumber}";
+
+            DataContext = ViewModel;
+            DG1.Items.Refresh();
+        }
+
+
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ CustomerList.WPF/ViewModels/CustomerListPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerList.WPF.ViewModels
+{
+    public class CustomerListPageViewModel : BindableBase
+    {
+
+        /// Creates a new CustomerListPageViewModel.
+        public CustomerListPageViewModel() => Task.Run(GetCustomerListAsync);
+
+        /// The collection of customers in the list. 
+        public ObservableCollection<CustomerViewModel> Customers { get; }
+            = new ObservableCollection<CustomerViewModel>();
+
+        /// Gets the complete list of customers from the database.
+        public async Task GetCustomerListAsync()
+        {
+          
+
+            var customers = await App.Repository.Customers.GetAsync();
+
+            if (customers == null)
+            {
+                return;
+            }
+
+           
+                Customers.Clear();
+                foreach (var c in customers)
+                {
+                    Customers.Add(new CustomerViewModel(c));
+                }
+               
+            
+        }
+
+        /// Queries the database for a current list of customers.
+        public void Sync()
+        {
+
+            Task.Run(async () =>
+            {
+              
+                foreach (var modifiedCustomer in Customers
+                    .Where(customer => customer.IsModified).Select(customer => customer.Model))
+                {
+                    await App.Repository.Customers.UpsertAsync(modifiedCustomer);
+                }
+
+                await GetCustomerListAsync();
+               
+            });
+
+
+        }
+    }
+}
